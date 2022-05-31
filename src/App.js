a927e65ced2b385fc14e171eee84bed90a3255cd
Facundo Fontanella <38475690+Morta-Fontanella@users.noreply.github.com>
2022-05-30 23:30:36 -0300

    Added basic html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React from "react";
 
+import Container from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
+import Grow from "@mui/material/Grow";
+import Grid from "@mui/material/Grid";
+
+import Notes from "./components/notes/notes";
+import Form from "./components/form/form";
+
+import logo from "./images/logo.png";
+
 const App = () => {
 	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
+		<Container maxWidth="lg">
+			<AppBar position="static" color="inherit">
+				<img src={logo} />
+				<Typography variant="h2" color="inherit">
+					Remember
+				</Typography>
+			</AppBar>
+			<Grow in>
+				<Container>
+					<Grid
+						container
+						justify="space-between"
+						alignItems="stretch"
+						spacing={3}
+					>
+						<Grid item xs={12} sm={7}>
+							<Notes />
+						</Grid>
+						<Grid item xs={12} sm={4}>
+							<Form />
+						</Grid>
+					</Grid>
+				</Container>
+			</Grow>
+		</Container>
 	);
 };
 
---
+++ src/components/notes/notes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Note from "./note/note";
+
+const Notes = () => {
+	return (
+		<>
+			<div>
+				<h1>Notes</h1>
+				<Note />
+				<Note />
+			</div>
+		</>
+	);
+};
+
+export default Notes;
