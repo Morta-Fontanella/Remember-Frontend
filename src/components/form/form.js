2d48ff0f19a0439bbd1fb109f9b64e16cb463822
Facundo Fontanella <38475690+Morta-Fontanella@users.noreply.github.com>
2022-06-02 00:34:31 -0300

    Added style to form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,9 +1,27 @@
 import React from "react";
 
+import "./formStyles.css";
+
 const Form = () => {
 	return (
-		<div>
-			<h1>Form</h1>
+		<div className="formContainer hidden">
+			<form>
+				<div className="textContainer">
+					<div className="titleContainer">
+						<input className="title" type={Text} placeholder="Title"></input>
+						<i class="fa-solid fa-image"></i>
+					</div>
+					<textarea
+						className="content"
+						type={Text}
+						placeholder="Write something..."
+					></textarea>
+				</div>
+				<div className="buttonContainer">
+					<i class="fa-solid fa-palette"></i>
+					<i class="fa-solid fa-check"></i>
+				</div>
+			</form>
 		</div>
 	);
 };
