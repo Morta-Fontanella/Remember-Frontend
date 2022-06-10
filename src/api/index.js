e3c04f5b5f9e87aecf23e3c81b3402cf5f944af0
Facundo Fontanella <38475690+Morta-Fontanella@users.noreply.github.com>
2022-06-09 23:25:07 -0300

    Axios .Post bug fixed

--- src/api/index.js
+++ src/api/index.js
@@ -3,4 +3,4 @@ import axios from "axios";
 const url = "http://localhost:5000/notes";
 
 export const fetchNotes = () => axios.get(url);
-export const createNote = (newNote) => axios.note(url, newNote);
+export const createNote = (newNote) => axios.post(url, newNote);
