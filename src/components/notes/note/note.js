dfc40c09e57661bade4aec7523fdb8902917bb04
Facundo Fontanella <38475690+Morta-Fontanella@users.noreply.github.com>
2022-06-10 01:21:39 -0300

    Added toggle buttons

--- src/components/notes/note/note.js
+++ src/components/notes/note/note.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 
 import "./noteStyles.css";
 
 function Note({ note }) {
+	const [openColors, setOpenColors] = useState(false);
+	const toggleColors = () => setOpenColors(!openColors);
+	const [openOptions, setOpenOptions] = useState(false);
+	const toggleOptions = () => setOpenOptions(!openOptions);
+
 	return (
 		<div className={"note " + (note.color ? note.color : "")}>
 			{note.image ? <img src={note.image} alt="note" /> : <div></div>}
@@ -17,8 +22,36 @@ function Note({ note }) {
 						Created by {note.creator} {moment(note.createdAt).fromNow()}
 					</p>
 					<div className="buttonContainer">
-						<i className="fa-solid fa-palette"></i>
-						<i className="fa-solid fa-ellipsis-vertical"></i>
+						<i
+							className="fa-solid fa-palette"
+							onKeyPress={() => toggleColors(!openColors)}
+							onClick={() => toggleColors(!openColors)}
+						></i>
+						{openColors ? (
+							<div className="colorPicker">
+								<div className="color red"></div>
+								<div className="color pink"></div>
+								<div className="color yellow"></div>
+								<div className="color green"></div>
+								<div className="color blue"></div>
+								<div className="color purple"></div>
+							</div>
+						) : null}
+						<i
+							className="fa-solid fa-ellipsis-vertical"
+							onKeyPress={() => toggleOptions(!openColors)}
+							onClick={() => toggleOptions(!openColors)}
+						></i>
+						{openOptions ? (
+							<div className="dropdownOptions">
+								<span className="edit" onClick={() => console.log("edit")}>
+									Edit
+								</span>
+								<span className="option" onClick={() => console.log("delete")}>
+									Delete
+								</span>
+							</div>
+						) : null}
 					</div>
 				</div>
 			</div>
