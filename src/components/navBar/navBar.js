9a4e5ad4c3c69d2f7aa8e38f8f8dfb2057276996
Facundo Fontanella <38475690+Morta-Fontanella@users.noreply.github.com>
2022-07-10 22:08:59 -0300

    Added dark mode

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -6,6 +6,28 @@ import Button from "../button/button";
 import "./navBarStyles.css";
 
 const NavBar = () => {
+	var storedTheme =
+		localStorage.getItem("theme") ||
+		(window.matchMedia("(prefers-color-scheme: dark)").matches
+			? "dark"
+			: "light");
+	if (storedTheme)
+		document.documentElement.setAttribute("data-theme", storedTheme);
+
+	var currentTheme = document.documentElement.getAttribute("data-theme");
+
+	const changeMode = () => {
+		var currentTheme = document.documentElement.getAttribute("data-theme");
+		var targetTheme = "light";
+
+		if (currentTheme === "light") {
+			targetTheme = "dark";
+		}
+
+		document.documentElement.setAttribute("data-theme", targetTheme);
+		localStorage.setItem("theme", targetTheme);
+	};
+
 	return (
 		<header>
 			<NavLink to="/home">
@@ -14,13 +36,16 @@ const NavBar = () => {
 					<h2>Remember</h2>
 				</div>
 			</NavLink>
-			<div className="userButtonsContainer">
-				<NavLink to="/login">
-					<Button type="outlined">Sign in</Button>
-				</NavLink>
-				<NavLink to="/register">
-					<Button type="filled">Sign up</Button>
-				</NavLink>
+			<div className="rightContainer">
+				<i className="fa-solid fa-moon " onClick={changeMode}></i>
+				<div className="userButtonsContainer">
+					<NavLink to="/login">
+						<Button type="outlined">Sign in</Button>
+					</NavLink>
+					<NavLink to="/register">
+						<Button type="filled">Sign up</Button>
+					</NavLink>
+				</div>
 			</div>
 		</header>
 	);
