96f7663bb26a7dc7c267d8a2ac0b3a735e084eb5
Facundo Fontanella <38475690+Morta-Fontanella@users.noreply.github.com>
2022-07-13 23:24:10 -0300

    Removed axios API

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-//const url = "http://localhost:5000/notes";
-const API = axios.create({ baseURL: "http://localhost:5000/notes" });
+const url = "http://localhost:5000/notes";
 
-export const fetchNotes = () => axios.get(API);
-export const createNote = (newNote) => axios.post(API, newNote);
+export const fetchNotes = () => axios.get(url);
+export const createNote = (newNote) => axios.post(url, newNote);
 export const updateNote = (id, updateNote) =>
-	axios.patch(`${API}/${id}`, updateNote);
-export const deleteNote = (id) => axios.delete(`${API}/${id}`);
+	axios.patch(`${url}/${id}`, updateNote);
+export const deleteNote = (id) => axios.delete(`${url}/${id}`);
 
-export const signIn = (formData) => API.post("/user/signin", formData);
-export const signUp = (formData) => API.post("/user/signup", formData);
+export const signIn = (formData) => url.post("/user/signin", formData);
+export const signUp = (formData) => url.post("/user/signup", formData);
